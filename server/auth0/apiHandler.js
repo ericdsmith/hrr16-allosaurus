b04cdaf09247571bfd6566695619096637e3fb73
ericdsmith <tech.ericsmith@gmail.com>
2016-10-25 17:13:50 -0600

    updated api keys to environment and changed to tech.ericsmith auth0 account

--- server/auth0/apiHandler.js
+++ server/auth0/apiHandler.js
@@ -1,14 +1,14 @@
 var auth0 = require('./authentication');
 var request = require('request');
 
-//header information 
+//header information
 var header = {
-  uri: 'https://allosaurus.auth0.com/api/v2/users?fields=user_id,name',
+  uri: process.env.AUTH0_DOMAIN + '/api/v2/users?fields=user_id,name',
   method: 'GET',
   headers: {
     Authorization: auth0.AUTH0_AUTHENTICATION
-  } 
-}; 
+  }
+};
 
 //gets all users that logged in using Auth0
 exports.getAllUsers = function(callback){
